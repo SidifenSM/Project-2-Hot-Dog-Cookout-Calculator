cf68b1a179c0931edf4ffab993e19a821e7f30e1
SidifenSM <114959980+SidifenSM@users.noreply.github.com>
2022-11-07 23:04:50 -0500

    Update Project_2_Hot_Dog_Cookout_Calculator.py

--- Project 2 Hot Dog Cookout Calculator/Project_2_Hot_Dog_Cookout_Calculator.py
+++ Project 2 Hot Dog Cookout Calculator/Project_2_Hot_Dog_Cookout_Calculator.py
@@ -18,42 +18,36 @@
 hot_dogs = 10
 hot_buns = 8
 
-def hotdogleaders (a,b):
-    a = a // b
-    return a
+people = int(input("How many people will be attending the cookout: "))
+give_dog = int(input("How many hot dogs will each person be given: "))
 
+total_doggers = people * give_dog
+
+#div mod as an alternative to avoid having to use // or %
 def hotdogowners (apple,orange):
-    apple = apple % orange
+    apple = divmod(apple, orange)
     return apple
 
 def ketchup (fruit, mango):
-    fruit = fruit * mango
+    fruit = fruit - mango
     return fruit
 
-people = int(input("How many people will be attending the cookout: "))
+#taking two arguments
+hotdog_package, dog_leftover = hotdogowners(total_doggers, hot_dogs)
+buns_needed, bun_leftover = hotdogowners(total_doggers, hot_buns)
 
-give_dog = int(input("How many hot dogs will each person be given: "))
-
-total_doggers = ketchup(people,give_dog)
-
-hotdog_package = hotdogleaders(total_doggers, hot_dogs)
-buns_needed = hotdogleaders(total_doggers, hot_buns)
-
-if hotdogowners(total_doggers, hot_buns):
-    buns_needed += 1
-
-if hotdogowners(total_doggers, hot_dogs):
+if dog_leftover != 0:
     hotdog_package += 1
+    dog_leftover = ketchup(hot_dogs,dog_leftover)
 
-total_buns = ketchup(buns_needed,hot_buns)
+if bun_leftover != 0:
+    buns_needed += 1
+    bun_leftover = ketchup(hot_buns,bun_leftover)
 
-dog_leftover = hotdogowners(total_doggers, hot_dogs)
-#dog_leftover = total_doggers - hot_dogs
-bun_leftover = total_buns - total_doggers
-#bun_leftover = hotdogowners(total_doggers, hot_buns)
 
 print("\n")
 print("Minimum number of packages of hot dogs required: ", hotdog_package)
 print("Minimum number of packages of hot dog buns required: ", buns_needed)
 print("The number of hot dogs that will be left over: ", dog_leftover)
 print("The number of hot dog buns that will be left over: ", bun_leftover)
+print("\n")
